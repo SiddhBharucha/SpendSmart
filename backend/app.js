6d79a6c5ff9331a2d071eb240ef96fa79aad69cc
gregcm03 <unniat03@gmail.com>
2023-04-15 02:19:43 -0500

    updated

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { db } = require('./db/db');
-
+const {readdirSync} = require('fs')
 require('dotenv').config();
 
 const PORT = process.env.PORT;
@@ -10,6 +10,9 @@ const PORT = process.env.PORT;
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// routes 
+readdirSync('./routes').map((route)=> app.use('/api/v1', require('./routes/'+ route)))
 
 db().then(() => {
   // Start the server only after the database connection is established
