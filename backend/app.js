e55b8b9d037eace0e3a65d271992d85deb7980dd
gregcm03 <unniat03@gmail.com>
2023-04-14 22:53:25 -0500

    update

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+
 require('dotenv').config()
 
 const PORT = process.env.PORT
 
+// middleware
+app.use(express.json())
+app.use(cors())
+
+
 const server = () => {
-  console.log('You are listening tp port:', PORT);
+  db()
+  app.listen(PORT, () => {
+    console.log('listening to port:', PORT)
+  })
 }
 
 server()
