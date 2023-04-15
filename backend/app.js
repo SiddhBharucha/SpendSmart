6f80fe7f372a2cafdb2a322a50fa3944d7b79ecb
gregcm03 <unniat03@gmail.com>
2023-04-15 00:46:51 -0500

    update

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,21 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const app = express()
+const app = express();
+const { db } = require('./db/db');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-
-const server = () => {
-  db()
+db().then(() => {
+  // Start the server only after the database connection is established
   app.listen(PORT, () => {
-    console.log('listening to port:', PORT)
-  })
-}
-
-server()
+    console.log('listening to port:', PORT);
+  });
+}).catch((error) => {
+  console.error('DB Connection Error:', error);
+});
